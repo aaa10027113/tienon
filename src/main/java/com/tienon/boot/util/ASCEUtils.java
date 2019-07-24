f2e2fd6782eef0bce6029802002aad467f183357
suzhoutienon <hanhao@tienon-sh.com>
2019-07-24 14:32:52 +0800

    加密工具类

---
+++ src/main/java/com/tienon/boot/util/ASCEUtils.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright by www.tienon.com
+ * All right reserved.
+ */
+package com.tienon.boot.util;
+
+/**
+ * @author zouhuaqiang
+ * @Description TODO
+ * @date 2019/07/24日
+ */
+public class ASCEUtils {
+    private static ASCEncrypt ascEncrypt = new ASCEncrypt("123456");
+
+    public static String encrypt(String msg) {
+        return ascEncrypt.encrypt(msg);
+    }
+
+    public static String decrypt(String msg) {
+        return ascEncrypt.decrypt(msg);
+    }
+
+    public static void main(String[] args) {
+        String msg = "今天天气不错啊";
+        String after = encrypt(msg);
+        System.out.println("密文：" + after);
+        System.out.println("明文：" + decrypt(after));
+    }
+}
