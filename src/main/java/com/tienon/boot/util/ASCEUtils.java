33f938b2661fff63be1512d3be58d280383c8e5e
suzhoutienon <hanhao@tienon-sh.com>
2019-07-24 14:35:09 +0800

    加密工具类

--- src/main/java/com/tienon/boot/util/ASCEUtils.java
+++ src/main/java/com/tienon/boot/util/ASCEUtils.java
@@ -4,13 +4,15 @@
  */
 package com.tienon.boot.util;
 
+import com.tienon.boot.constant.CommonStatic;
+
 /**
  * @author zouhuaqiang
  * @Description TODO
  * @date 2019/07/24日
  */
 public class ASCEUtils {
-    private static ASCEncrypt ascEncrypt = new ASCEncrypt("123456");
+    private static ASCEncrypt ascEncrypt = new ASCEncrypt(CommonStatic.KEY);
 
     public static String encrypt(String msg) {
         return ascEncrypt.encrypt(msg);
