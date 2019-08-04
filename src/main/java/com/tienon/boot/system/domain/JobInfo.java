80828e88e211a7f9ffb407908ed9e64da4b49843
谢永强 <@ST-201610051444>
2019-08-04 20:42:26 +0800

    提交代码

---
+++ src/main/java/com/tienon/boot/system/domain/JobInfo.java
@@ -0,0 +1,157 @@
+/**
+ * Copyright by www.tienon.com
+ * All right reserved.
+ */
+
+package com.tienon.boot.system.domain;
+
+/**
+ * @Description TODO(定时器domain)
+ * 
+ * @author lilei
+ * @date 2019/08/04
+ */
+public class JobInfo {
+	/**
+	 * 日志id
+	 */
+	private Long logId;
+
+	/**
+	 * 任务id
+	 */
+	private Long jobId;
+
+	/**
+	 * spring bean名称
+	 */
+	private String beanName;
+
+	/**
+	 * 方法名
+	 */
+	private String methodName;
+
+	/**
+	 * 参数
+	 */
+	private String params;
+
+	/**
+	 * 任务状态 0：成功 1：失败
+	 */
+	private String status;
+
+	/**
+	 * 失败信息
+	 */
+	private String error;
+
+	/**
+	 * 耗时(单位：毫秒)
+	 */
+	private Integer times;
+
+    /**
+     * 创建时间
+     */
+    private String creatDate;
+    
+    /**
+     * 更新时间
+     */
+    private String updateDate;
+
+    /**
+     * 操作员
+     */
+    private String operator;
+
+	public Long getLogId() {
+		return logId;
+	}
+
+	public void setLogId(Long logId) {
+		this.logId = logId;
+	}
+
+	public Long getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(Long jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getBeanName() {
+		return beanName;
+	}
+
+	public void setBeanName(String beanName) {
+		this.beanName = beanName;
+	}
+
+	public String getMethodName() {
+		return methodName;
+	}
+
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+
+	public String getParams() {
+		return params;
+	}
+
+	public void setParams(String params) {
+		this.params = params;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public Integer getTimes() {
+		return times;
+	}
+
+	public void setTimes(Integer times) {
+		this.times = times;
+	}
+
+	public String getCreatDate() {
+		return creatDate;
+	}
+
+	public void setCreatDate(String creatDate) {
+		this.creatDate = creatDate;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public String getOperator() {
+		return operator;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+}
---
+++ src/main/java/com/tienon/boot/system/service/JobService.java
@@ -0,0 +1,116 @@
+package com.tienon.boot.system.service;
+
+import com.alibaba.fastjson.JSON;
+import com.tienon.EjxError;
+import com.tienon.boot.system.domain.JobInfo;
+import com.tienon.boot.system.mapper.JobMapper;
+import com.tienon.boot.util.support.PageGrid;
+import com.tienon.boot.util.support.PageResult;
+import com.tienon.framework.persistence.mybatis.paginator.domain.PageBounds;
+import com.tienon.framework.persistence.mybatis.paginator.domain.PageList;
+import com.tienon.framework.supports.ActionResult;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class JobService {
+	 private static Logger log = Logger.getLogger(JobService.class);
+	 
+    @Autowired
+    JobMapper jobMapper;
+    
+    /**
+     * TODO(查询定时任务列表)
+     * 
+     * @param pg
+     * @return 
+     * @return Object 返回类型
+     */
+    public Object queryList(PageGrid pg) {
+        int page = pg.getPage();
+        int pageSize = pg.getRows();
+        PageBounds pageBounds = new PageBounds(page, pageSize, true);
+
+        try {
+            PageList<JobInfo> pageList = jobMapper.queryJobList(pg.getSearchCondition(), pageBounds);
+            int total = pageList.getPaginator().getTotalCount();
+            return new ActionResult(new PageResult(total, pageList));
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("查询查询定时任务列表出现异常[" + e.getMessage() + "]");
+            return new ActionResult(false, "查询查询定时任务列表出现异常！");
+        }
+    }
+    
+    /**
+     * TODO(保存定时任务)
+     * 
+     * @param info
+     * @return 
+     * @return Object 返回类型
+     */
+    public Object addJobInfo(JobInfo info) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = new Date();
+            info.setCreatDate(sdf.format(date));
+            info.setUpdateDate(sdf.format(date));
+            info.setStatus("0");
+            jobMapper.addJobInfo(info);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("保存定时任务出现异常" + e.getMessage() + "]");
+            return new ActionResult(false, "保存定时任务出现异常！");
+        }
+        return new ActionResult(true);
+    }
+    
+    /**
+     * TODO(根据ID，查询定时任务)
+     * 
+     * @param id
+     * @return 
+     * @return Object 返回类型
+     */
+    public Object queryInfoById(String id) {
+        try {
+            JobInfo info = jobMapper.queryInfoById(id);
+            if (null == info) {
+                return new ActionResult(false, "根据ID，查询不到定时任务！");
+            }
+            return new ActionResult(true, "查询成功！", info);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("根据ID，查询定时任务出现异常" + e.getMessage() + "]");
+            return new ActionResult(false, "根据ID，查询定时任务出现异常！");
+        }
+    }
+    
+    /**
+     * TODO(更新定时任务)
+     * 
+     * @param info
+     * @return 
+     * @return Object 返回类型
+     */
+    public Object updateJobInfo(JobInfo info) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = new Date();
+            info.setUpdateDate(sdf.format(date));
+            info.setStatus("0");
+            jobMapper.updateJobInfo(info);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("更新定时任务出现异常" + e.getMessage() + "]");
+            return new ActionResult(false, "更新定时任务出现异常！");
+        }
+        return new ActionResult(true);
+    }
+}
